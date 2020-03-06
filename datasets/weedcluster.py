546467a7854090bb2dc2332d1a6e14b43e797921
Caroline <carolinepotts@msn.com>
2020-03-06 12:18:21 -0500

    prints pred

--- datasets/weedcluster.py
+++ datasets/weedcluster.py
@@ -34,9 +34,9 @@ class WeedClusterDataset(Dataset):
     def __getitem__(self, index):
         img = torch.from_numpy(np.array(Image.open(self.root + self.split +  '/images/rgb/' + self.images[index]).convert('RGB')).reshape((3,512,512)))
         # print("img shape: ", img.shape)
-        target = torch.from_numpy(np.array(io.imread(self.root + self.split +  '/labels/weed_cluster/' + self.images[index][:-4] + '.png')))
+        target = torch.from_numpy(np.array(Image.open(self.root + self.split +  '/labels/weed_cluster/' + self.images[index][:-4] + '.png')))
         # print("target shape: ", target.shape)
-        print("target: ", target)
+        # print("target: ", target)
 
         return img, target
 
